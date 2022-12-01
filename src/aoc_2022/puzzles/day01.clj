488900c812b42e73f503248a80e29f2ff9cae93b
phosfox <mordredur@googlemail.com>
2022-12-01 18:58:27 +0100

    init

---
+++ src/aoc_2022/puzzles/day01.clj
@@ -0,0 +1,32 @@
+(ns aoc-2022.puzzles.day01
+
+  (:require [clojure.string :as str]))
+
+
+(defn parse-int [n]
+  (Integer/parseInt n))
+
+(def input (slurp "resources/day01.txt"))
+
+
+
+(defn part-1 [input]
+  (->> (str/split input #"\n\n")
+       (map str/split-lines)
+       (map #(map parse-int %))
+       (map #(reduce + %))
+       (apply max)))
+
+(part-1 input)
+
+
+(defn part-2 [input]
+  (->> (str/split input #"\n\n")
+       (map str/split-lines)
+       (map #(map parse-int %))
+       (map #(reduce + %))
+       (sort >)
+       (take 3)
+       (reduce +)))
+
+(part-2 input)
